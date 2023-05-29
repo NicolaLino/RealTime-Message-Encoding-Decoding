d9be88d4ace684674bc7fdbd5035676d8a8b647e
RamiBarakat <ramibarakat522002@gmail.com>
2023-05-29 23:25:33 +0300

    Almost Done!

--- master_spy.c
+++ master_spy.c
@@ -1,9 +1,54 @@
+#include "header.h"
 #include "constants.h"
 #include "functions.h"
 
+
 int main(int argc, char **argv)
 {
-    printf("Master Spy process started\n");
+    
+    struct message msg;
+    int columns = atoi(argv[2]);
+    int bitmap[columns];
+    memset(bitmap, 0x0, sizeof(bitmap));
+    
+    key_t key2 = ftok(".", 'y');
+    int msgqSpy = msgget(key2, 0666);
+
+    if (msgqSpy == -1)
+    {
+        perror("msgget");
+        exit(-1);
+    }
+
+    while(!checkColumns(bitmap, columns)){
+
+        
+            if (msgrcv(msgqSpy, &msg, sizeof(msg.text), 1, 0) == -1)
+            {
+            perror("msgrcv");
+            exit(-1);
+            }
+        
+
+        
+        //If column is already read, then get another index
+        if (bitmap[(int)(msg.text[0])] == 1)
+        {
+            continue;
+        }
+
+        //set column to read
+        bitmap[(int)(msg.text[0])] = 1;
+        printf("IN MASTERSPY %s\n",msg.text);
+        fflush(stdout);
+        
+
+    }
+
+    printf("GOT ALL COLUMNS IN MASTER_SPY :(");
+    fflush(stdout);
+
+
 
     return 0;
 }
--- spy.c
+++ spy.c
@@ -4,6 +4,53 @@
 int main(int argc, char **argv)
 {
     printf("Spy process started\n");
+    int shmid = open_shmem();
+    int semid = open_sem();
+    int columns = atoi(argv[2]);
+    int value;
+    struct message msgSP;
+    
+
+    srand(time(0) * 5.333 * getpid());
+
+    //msg Q between the spies and the master spy
+    key_t key2 = ftok(".", 'y');
+    int msgqSpy = msgget(key2, 0666);
+
+    if (msgqSpy == -1)
+    {
+        perror("msgget");
+        exit(-1);
+    }
+
+   int i = 0;
+    while(i < 5)
+    {
+        i++;
+        value = (int)(rand() % columns);
+        usleep(7000000);
+        lock(semid);
+
+        char *shared_data = attachSharedMemory(shmid);
+
+        printf("String received from shared memory in Spy: %s\n", shared_data + (value * 100));
+
+        msgSP.type = 1;
+        strcpy(msgSP.text, shared_data + (value * 100) );
+        if (msgsnd(msgqSpy, &msgSP, sizeof(msgSP.text) - sizeof(long), 0) == -1)
+            { 
+                perror("msgsnd");
+                exit(-1);
+            }
+            else{
+                printf("%s\n", msgSP.text);
+                fflush(stdout);
+            }
+        
+
+        unlock(semid);
+        shmdt(shared_data);
+    }
 
     return 0;
 }
